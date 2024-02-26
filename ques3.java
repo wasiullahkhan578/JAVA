f127095d0439cb944d0d811e026fb3274dc9218f
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-02-26 13:27:33 +0530

    bill

---
+++ ques3.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class ques3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float pen = sc.nextFloat();
+        float pencil = sc.nextFloat();
+        float eraser = sc.nextFloat();
+        float bill = pen+pencil+eraser;
+        System.out.println(bill);
+    }
+}
