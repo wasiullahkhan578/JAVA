a4dab7f3904fba79fa494f6ad7d5269a653024c4
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-02-26 15:35:50 +0530

    IfElse

---
+++ IfElse.java
@@ -0,0 +1,11 @@
+public class IfElse {
+    public static void main(String[] args) {
+    int a = 10;
+    int b = 15;
+    if(a>b) {
+        System.out.println("A is Greater");
+    }   else{
+        System.out.println("B is Greater");
+    }
+}
+}
