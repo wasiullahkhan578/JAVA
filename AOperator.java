c4113ca7e0b1cc68d8f33348a276e7ec7ae7d8af
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-02-26 14:09:16 +0530

    Aoperator

---
+++ AOperator.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class AOperator {
+    public static void main(String[] args) {
+        int a=40;
+        int b=30;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b)
+    }
+}
+
