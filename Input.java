d5415593e297be88b1e56cd81721ba39f25e0e24
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-02-26 09:22:06 +0530

    Input

---
+++ Input.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+            String name = sc.nextLine ();
+            System.out.println(name);
+            Scanner scan = new Scanner (System.in);
+            int Age = sc.nextInt ();
+            System.out.println(Age);
+    }
+}
