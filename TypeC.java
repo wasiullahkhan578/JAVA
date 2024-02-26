371876bb45b83bd853f043ce9610ce3a984b8365
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-02-26 12:43:41 +0530

    Type Conversion

---
+++ TypeC.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class TypeC {
+    public static void main(String args []){
+        Scanner sc = new Scanner(System.in);
+        float num = sc.nextInt();
+        System.out.println(num);
+    }
+    
+}
