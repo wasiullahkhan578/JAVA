29783c4e49687b399c0dd47ada3a0cacda4674eb
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-02-26 13:21:48 +0530

    Area

---
+++ ques2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class ques2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        int area = r*r;
+        System.out.println(area);
+
+    }
+}
