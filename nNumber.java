c37c5d83483b28da55740f37ae57682afa2751f2
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-02-27 19:34:39 +0530

    num2

---
+++ nNumber.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class nNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = 1;
+        while (m<=n){
+            System.out.print(m);
+            m++;
+        }
+    }
+}
