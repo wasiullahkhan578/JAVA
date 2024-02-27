f3600b51b9eab428412e275ee4c0c18eaa0a0f6d
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-02-27 20:24:16 +0530

    forloop

---
+++ forloop.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class forloop {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int i;
+        for(i=1;i<=num;i++){
+            System.out.println(i);
+        }
+    }
+}
