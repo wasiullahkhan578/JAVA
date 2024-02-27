3c5918336c35b1e6410e4d64b29460a6252811de
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-02-27 23:40:15 +0530

    reverse

---
+++ pattern.java
@@ -0,0 +1,10 @@
+public class pattern {
+    public static void main(String[] args) {
+        int i;
+        for(i=1; i<=4; i++){
+            System.out.println("****");
+
+        }
+    }
+    
+}
