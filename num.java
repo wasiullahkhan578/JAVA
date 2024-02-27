e8e4ac2109650af0e67e2cf0e88d81eaa9952ce2
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-02-27 19:22:55 +0530

    number

---
+++ num.java
@@ -0,0 +1,9 @@
+public class num {
+    public static void main(String[] args) {
+        int num = 0;
+        while(num<=10){
+            System.out.println(num);
+            num++;
+        }
+    }
+}
