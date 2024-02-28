337b0a74e2dafd7f5080897f1cb39e9927e46dc0
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-02-28 09:19:13 +0530

    continue

---
+++ continue.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class continue {
+    public static void main(String args[]){
+        
+        for(i=1;i<=10;i++){
+            if(i==5){
+                continue;
+            }
+        }System.out.println(i);
+    }
+}
