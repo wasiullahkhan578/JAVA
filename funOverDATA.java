36146e481d985e168e6ad62b5baf2d2b9d5a8ff9
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-02-28 19:04:51 +0530

    data type ovrloading

---
+++ funOverDATA.java
@@ -0,0 +1,12 @@
+public class funOverDATA {
+    public static int sum(int a,int b){
+        return a+b;
+    }
+    public static float sum(float a, float b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        System.out.println(sum(5,8));
+        System.out.println(sum(3.4f,5.6f));
+    }
+}
