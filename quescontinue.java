5f483b403af5c92c3171ae81e529af2ae2958329
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-02-28 09:29:15 +0530

    continue

---
+++ quescontinue.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class quescontinue {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        do{
+            System.out.println("Enter Your Number..");
+            int i = sc.nextInt();
+            if( i % 10 == 0){
+                continue;
+            } System.out.println(i);
+        } while(true);
+    }
+}
