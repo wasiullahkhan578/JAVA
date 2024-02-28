43d9c73617c0808eaece09e7ee67f3c162d660fb
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-02-28 07:35:31 +0530

    do while

---
+++ doWhile.java
@@ -0,0 +1,9 @@
+public class doWhile {
+    public static void main(String[] args) {
+        int counter = 1;
+        do{
+            System.out.println("Hello World");
+            counter++;
+        } while(counter <=10);
+    }
+}
