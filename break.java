0ae81953daf3bad6ad35bbc895d80ba00f44b7b7
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-02-28 07:41:27 +0530

    break

---
+++ break.java
@@ -0,0 +1,12 @@
+public class break {
+    public static void main(String args[]){
+        for(i=1; i<=10;i++){
+            if(i==7){
+                break;
+            }
+            System.out.println(i);
+           }
+           System.out.println("I Am Out Of Loop.");
+
+    }
+}
