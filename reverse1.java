926e4ec0568a0e350dd909197e0e0b039c5d2024
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-02-28 07:26:59 +0530

    Reverse

---
+++ reverse1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class reverse1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int rev = 0;
+        while(num>0){
+            int lastDigit =num%10;
+            rev = (rev*10)+lastDigit;
+            num = num/10;
+        }
+        System.out.println(rev);
+    }
+
+}
