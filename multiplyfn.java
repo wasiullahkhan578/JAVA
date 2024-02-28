e5b203b44220772b0d863830d3a97451bdd0d8e9
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-02-28 17:55:53 +0530

    mutiply by fn

---
+++ multiplyfn.java
@@ -0,0 +1,14 @@
+public interface multiplyfn {
+    public static int multiply(int a, int b){
+        int product = a*b;
+        return product;
+    }
+    public static void main(String[] args) {
+       int pro = multiply(5, 07);
+        System.out.println(pro);
+        int a = 10;
+        int b= 20;
+        int prod = multiply(a, b);
+        System.out.println(prod);
+    }
+}
