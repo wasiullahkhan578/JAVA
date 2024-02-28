bfe1b27cb30d0142fac1076b9c035c29fcd1d804
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-02-28 09:10:55 +0530

    multipler

---
+++ quesdo.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class quesdo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        do{
+            System.out.println("Enter Your Number.");
+            int n = sc.nextInt();
+            if(n%10==0){
+                break;
+            }System.out.println(n);
+
+        }while(true); 
+    }
+
+}
