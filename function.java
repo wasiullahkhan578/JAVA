33be509b28be9df570a167c890160c14fe249ea9
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-02-28 16:27:53 +0530

    funtion

---
+++ function.java
@@ -0,0 +1,8 @@
+public class function {
+    public static void Hello(){
+        System.out.println("Hello World. ");
+    }
+    public static void main (String args[]){
+        Hello();
+    }
+}
