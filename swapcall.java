e590f2394c3c2f831af5ab8eab83bf1ebd348680
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-02-28 17:44:54 +0530

    swap

---
+++ swapcall.java
@@ -0,0 +1,15 @@
+public class swapcall {
+    public static void swap(int a,int b){
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("a is "+a);
+        System.out.println("b is "+b);
+    }
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        swap (a,b);
+        
+    }
+}
