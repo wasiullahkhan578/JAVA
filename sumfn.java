8724c1042589e1a16f6e5d30d8c0d2768cca47f3
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-02-28 16:45:27 +0530

    fn

--- sumfn.java
+++ sumfn.java
@@ -1,13 +1,13 @@
 import java.util.*;
 public class sumfn {
-    public static void sum(){
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int add = a+b;
+    public static void sum(int num1 , int num2){
+        int add = num1+num2;
         System.out.println("the addition is  "+ add);
     }
     public static void main(String[] args) {
-        sum();
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sum(a,b);
     }
 }
