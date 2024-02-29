9bb1b496f00659dfcde5ab38bf3385ced5e88f93
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-02-29 11:12:28 +0530

    arrays

---
+++ Arrays1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Arrays1 {
+    public static void main(String[] args) {
+        int [ ] marks = new int [3];
+        marks [0] = 89;
+        marks [1] = 78;
+        marks [2] = 99;
+        for(int i =0; i<3;i++){
+            System.out.println(marks[i]);
+        }
+
+
+    }
+}
