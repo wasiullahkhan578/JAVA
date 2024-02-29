4fc0371b6a783259413f44740abc7b83020b35ff
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-02-29 12:17:19 +0530

    ques1 arrays

---
+++ arraysQues.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class arraysQues {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int numbers [] = new int [size];
+        for(int i = 0; i<size; i++){
+            numbers[i] = sc.nextInt();
+        } 
+        int x = sc.nextInt();
+        for(int i = 0; i<numbers.length; i++){
+            if(numbers[i]==x){
+                System.out.println("x is found at "+i);
+            }
+        }
+
+    }
+}
