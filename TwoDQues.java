4e1e61922af19b0bcc4dea96b8669c999dc9d830
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-02-29 14:31:18 +0530

    find X in 2 d array

---
+++ TwoDQues.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class TwoDQues {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+        int numbers [] [] = new int [rows][cols];
+        for(int i = 0; i<rows; i++){
+            for(int j = 0; j<cols; j++){
+                numbers[i][j] = sc.nextInt();
+            }
+        }
+        int x = sc.nextInt();
+        //output
+        for(int i =0; i<numbers.length; i++){
+            for(int j = 0; j<numbers.length; j++){
+                if(numbers[i][j]==x){
+                    System.out.println("x is found at "+i+","+j);
+                }
+            }
+        }
+    }
+
+}
