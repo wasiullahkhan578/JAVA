b959ccde58b86a15043ba883de0bf03a8c09b300
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-03-03 21:09:39 +0530

    testcase FIZZBUZZ

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -3,16 +3,19 @@ public class FizzBuzz {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        for(int i=1; i<=num; i++){
-            if(i%5==0 && i%3==0){
-                System.out.println("FizzBuzz");
-            }else if(i%5==0){
-                System.out.println("Buzz");
-            }else if(i%3==0){
-                System.out.println("Fizz");
-            }else{
-                System.out.println(i);
+        int test = sc.nextInt();
+        for( int j=0; j<test; j++){
+            int num = sc.nextInt();
+            for(int i=1; i<=num; i++){
+                if(i%5==0 && i%3==0){
+                    System.out.println("FizzBuzz");
+                }else if(i%5==0){
+                    System.out.println("Buzz");
+                }else if(i%3==0){
+                    System.out.println("Fizz");
+                }else{
+                    System.out.println(i);
+                }
             }
         }
     }
