cf7ac11ac8d532dbd3bbaddb3a7f64a9bd19809f
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-03-09 22:04:18 +0530

    ..

---
+++ fuzzbuzz2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class fuzzbuzz2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+    System.out.println("Enter your number: ");
+    int num = sc.nextInt();
+    if( num%3==0 && num%5==0){
+        System.out.println("FizzBuzz");
+    } else if( num%5==0){
+        System.out.println("Buzz");
+    } else if( num%3==0){
+        System.out.println("Fizz");
+    } else{
+        System.out.println(num);
+    }
+    }
+}
