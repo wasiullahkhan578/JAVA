1b4ec318fac9cdd51ee5f400da58df9565985021
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-03-10 21:18:31 +0530

    .

--- fuzzbuzz2.java
+++ fuzzbuzz2.java
@@ -2,18 +2,18 @@ import java.util.*;
 public class fuzzbuzz2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-    System.out.println("Enter your number: ");
-    int num = sc.nextInt();
-    for (int i=0; i<=num; i++){
-    if( num%3==0 && num%5==0){
-        System.out.println("FizzBuzz");
-    } else if( num%5==0){
-        System.out.println("Buzz");
-    } else if( num%3==0){
-        System.out.println("Fizz");
-    } else{
-        System.out.println(num);
-    }
-    }
+        System.out.println("Enter your number: ");
+        int num = sc.nextInt();
+            for(int i=1; i<=num; i++){
+                if(i%5==0 && i%3==0){
+                    System.out.println("FizzBuzz");
+                }else if(i%5==0){
+                    System.out.println("Buzz");
+                }else if(i%3==0){
+                    System.out.println("Fizz");
+                }else{
+                    System.out.println(i);
+                }
+        }
 }
 }
