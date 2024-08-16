e0629edb4bf76ff037f42a33e62e303839fd20a3
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-08-16 17:52:26 +0530

    change

--- forloop.java
+++ forloop.java
@@ -4,9 +4,12 @@ public class forloop {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
+        sc.close();
+
         int i;
         for(i=1;i<=num;i++){
             System.out.println(i);
         }
+
     }
 }
