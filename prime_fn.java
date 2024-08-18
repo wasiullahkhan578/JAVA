4eb419864b70e718fb0054c8cfb43435f2bc3610
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-08-18 21:17:15 +0530

    ,,

--- prime_fn.java
+++ prime_fn.java
@@ -1,7 +1,7 @@
 public class prime_fn {
     public static boolean isPrime(int n){
         boolean isPrime = true;
-        for(int i=2; i<=n-1; i++){
+        for(int i=2; i<=Math.sqrt(n); i++){
             if(n%i==0){
                 isPrime = false;
                 break;
