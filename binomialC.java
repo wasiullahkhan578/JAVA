4f6e17b747e0c079f3fd8f9a02e43a666313ddc8
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-08-18 18:11:37 +0530

    binomialCoefficient

---
+++ binomialC.java
@@ -0,0 +1,20 @@
+public class binomialC {
+    public static int factorial (int n){
+        int f = 1;
+        for(int i=1; i<=n; i++){
+            f= f*i;
+            
+        }
+        return f;
+    }
+    public static int bioCoeff( int n, int r){
+        int fact_n = factorial(n);
+        int fact_r = factorial(r);
+        int fact_nmr = factorial(n-r);
+        int bioCoeff = fact_n / (fact_r * fact_nmr);
+        return bioCoeff;
+    }
+    public static void main(String args[]){
+        System.out.println(bioCoeff(5, 2));
+    }
+}
