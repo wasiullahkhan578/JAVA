fb4fe145b7b06c8c3a6cd3d1f87126796a250272
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-08-27 21:25:02 +0530

    changes

--- Arrays2.java
+++ Arrays2.java
@@ -1,6 +1,6 @@
 public class Arrays2 {
     public static void main(String[] args) {
-        int [ ] marks = {45,79,56,43};
+        int [ ] marks = {45,89,56,43};
         for(int i = 0; i<4; i++){
             System.out.println(marks[i]);
         }
