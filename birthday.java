03e0ab8f2b9d65145ac7ad105be8079dd16ae9a5
Kaleem Akhtar Khan <kaleemakhtarkhan1@gmail.com>
2024-08-27 10:42:17 +0530

    Update birthday.java
    khus raho

--- birthday.java
+++ birthday.java
@@ -1,5 +1,6 @@
 public class birthday {
     public static void main(String[ ] args){
         System.out.println("Happy Birthday to me !");
+        system.out.println("Happy Birthday farhan! (This is from kaleem side !! )")
     }
 }
