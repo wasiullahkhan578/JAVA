bcba8fd72eed9ae4eff381d6d9151aa7ab96e186
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-08-27 21:16:22 +0530

    changes

--- Arrays1.java
+++ Arrays1.java
@@ -4,7 +4,7 @@ public class Arrays1 {
         marks [0] = 89;
         marks [1] = 78;
         marks [2] = 99;
-        marks [3] = 100;
+        marks [3] = 98;
         for(int i =0; i<4;i++){
             System.out.print(marks[i]);
         }
