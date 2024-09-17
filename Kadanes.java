fbdca066da6060f0a7bafa8e4a2792fb7828fd94
wasiullahkhan578 <140246355+wasiullahkhan578@users.noreply.github.com>
2024-09-17 14:57:35 +0530

    KADANE'S ALGORITHM

---
+++ Kadanes.java
@@ -0,0 +1,18 @@
+public class Kadanes {
+    public static void KAlgo( int num []){
+        int ms = Integer.MIN_VALUE;
+        int cs = 0;
+        for( int i=0; i<num.length; i++){
+            cs = cs + num[i];
+            if(cs< 0){
+                cs = 0;
+            }
+            ms = Math.max(cs, ms);
+        }
+        System.out.println(" our max subArray sum is : "+ms);
+    }
+    public static void main(String[] args) {
+        int num [] = {-2, -3, 4, -1, -2, 1, 5, -3};
+        KAlgo(num);
+    }
+}
